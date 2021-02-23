0cfbe25b8c9105ff00c966daa4316915ec7f0a5c
powerfulman86 <dba.ahmedmaher@gmail.com>
2021-02-23 20:13:23 +0200

    report

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from . import sale_net_product_detail_report
 from . import sale_return
 from . import sale_return_report
 from . import stock
+from . import sale_net_product_detail_report

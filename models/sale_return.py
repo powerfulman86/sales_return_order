c3a4334bb545a54e4a451b1646855c97cec8e49d
powerfulman86 <dba.ahmedmaher@gmail.com>
2021-10-05 13:41:18 +0200

    UPD

--- models/sale_return.py
+++ models/sale_return.py
@@ -7,7 +7,7 @@ from itertools import groupby
 
 from odoo import api, fields, models, SUPERUSER_ID, _
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools.misc import formatLang
+from odoo.tools.misc import formatLang, get_lang
 from odoo.osv import expression
 from odoo.tools import float_is_zero, float_compare
 
@@ -63,7 +63,7 @@ class SaleReturn(models.Model):
         domain=lambda self: [('groups_id', 'in', self.env.ref('sales_team.group_sale_salesman').id)])
 
     def _default_sale_id(self):
-        return  self.env['sale.order'].search([], limit=1)
+        return self.env['sale.order'].search([], limit=1)
 
     sale_id = fields.Many2one("sale.order", string="Sale Order", )
 
@@ -77,23 +77,27 @@ class SaleReturn(models.Model):
         states={'draft': [('readonly', False)]},
         required=True, change_default=True, index=True, tracking=1,
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", )
-
+    pricelist_id = fields.Many2one(
+        'product.pricelist', string='Pricelist', check_company=True,
+        required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+        help="If you change the pricelist, only newly added lines will be affected.")
     analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic Account')
     commitment_date = fields.Datetime('Delivery Date',
-                    states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
-                    copy=False, readonly=True,
-                    help="This is the delivery date promised to the customer. "
-                         "If set, the delivery order will be scheduled based on "
-                         "this date rather than product lead times.")
+                                      states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+                                      copy=False, readonly=True,
+                                      help="This is the delivery date promised to the customer. "
+                                           "If set, the delivery order will be scheduled based on "
+                                           "this date rather than product lead times.")
 
     def _get_invoiced(self):
         for rec in self:
             rec.invoice_count = len(rec.invoice_ids.ids)
 
     invoice_count = fields.Integer(string='Invoice Count', readonly=True)
-    invoice_ids = fields.Many2many("account.move", string='Invoices', readonly=True,copy=False)
+    invoice_ids = fields.Many2many("account.move", string='Invoices', readonly=True, copy=False)
     note = fields.Text('Terms and conditions')
-    amount_untaxed = fields.Float(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all',tracking=5)
+    amount_untaxed = fields.Float(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all', tracking=5)
     amount_tax = fields.Float(string='Taxes', store=True, readonly=True, compute='_amount_all')
     amount_total = fields.Float(string='Total', store=True, readonly=True, compute='_amount_all', tracking=4)
 
@@ -151,7 +155,7 @@ class SaleReturn(models.Model):
         journal = self.env['account.journal'].search([('type', '=', 'sale')], limit=1)
         if not journal:
             raise UserError(_('Please define an accounting sales journal for the company %s (%s).') % (
-            self.company_id.name, self.company_id.id))
+                self.company_id.name, self.company_id.id))
 
         lines = []
         for order_line in self.order_line:
@@ -201,8 +205,6 @@ class SaleReturn(models.Model):
             self.warehouse_id = warehouse_id or self.env['stock.warehouse'].search(
                 [('company_id', '=', self.company_id.id)], limit=1)
 
-
-
     @api.model
     def _default_warehouse_id(self):
         company = self.env.company.id
@@ -215,9 +217,9 @@ class SaleReturn(models.Model):
     #     company = self.env.company.id
     #     warehouse_ids = self.env['stock.warehouse'].search([('company_id', '=', company)], limit=1)
     #     return warehouse_ids
-        # company = self.env.user.company_id.id
-        # warehouse_ids = self.env['stock.warehouse'].search([], limit=1)
-        # return warehouse_ids
+    # company = self.env.user.company_id.id
+    # warehouse_ids = self.env['stock.warehouse'].search([], limit=1)
+    # return warehouse_ids
 
     warehouse_id = fields.Many2one(
         'stock.warehouse', string='Warehouse',
@@ -258,8 +260,15 @@ class SaleReturn(models.Model):
             else:
                 vals['name'] = self.env['ir.sequence'].next_by_code('sale.return', sequence_date=seq_date) or _('New')
 
+            # Makes sure 'pricelist_id' are defined
+            if any(f not in vals for f in ['pricelist_id']):
+                partner = self.env['res.partner'].browse(vals.get('partner_id'))
+                vals['pricelist_id'] = vals.setdefault('pricelist_id',
+                                                       partner.property_product_pricelist and partner.property_product_pricelist.id)
+
         if 'warehouse_id' not in vals and 'company_id' in vals and vals.get('company_id') != self.env.company.id:
-            vals['warehouse_id'] = self.env['stock.warehouse'].search([('company_id', '=', vals.get('company_id'))], limit=1).id
+            vals['warehouse_id'] = self.env['stock.warehouse'].search([('company_id', '=', vals.get('company_id'))],
+                                                                      limit=1).id
 
         return super(SaleReturn, self).create(vals)
 
@@ -349,7 +358,7 @@ class SaleReturn(models.Model):
                     if line.product_id == s_line.product_id and line.product_uom_qty > s_line.product_uom_qty:
                         raise ValidationError(_(
                             "Can not return Quantity more than [ %s ] sold for product [ %s ]" % (
-                            s_line.product_uom_qty, line.product_id.name)))
+                                s_line.product_uom_qty, line.product_id.name)))
 
         return True
 
@@ -430,7 +439,7 @@ class SaleReturn(models.Model):
 
     def action_view_credit_note(self):
         view_id = self.env.ref('account.view_move_form').id
-        print(">>>>>>>>>>>>>> ", self.invoice_ids.id)
+        # print(">>>>>>>>>>>>>> ", self.invoice_ids.id)
         return {
             'name': _('View Credit Note'),
             'type': 'ir.actions.act_window',
@@ -462,6 +471,37 @@ class SaleReturn(models.Model):
         for rec in self:
             rec.credit_note_count = len(rec.move_ids.ids)
 
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        """
+        Update the following fields when the partner is changed:
+        - Pricelist
+        - Payment terms
+        - Invoice address
+        - Delivery address
+        - Sales Team
+        """
+
+        partner_user = self.partner_id.user_id or self.partner_id.commercial_partner_id.user_id
+        values = {
+            'pricelist_id': self.partner_id.property_product_pricelist and self.partner_id.property_product_pricelist.id or False,
+        }
+        user_id = partner_user.id
+        if not self.env.context.get('not_self_saleperson'):
+            user_id = user_id or self.env.uid
+        if user_id and self.user_id.id != user_id:
+            values['user_id'] = user_id
+
+        if self.env['ir.config_parameter'].sudo().get_param(
+                'account.use_invoice_terms') and self.env.company.invoice_terms:
+            values['note'] = self.with_context(lang=self.partner_id.lang).env.company.invoice_terms
+        if not self.env.context.get('not_self_saleperson') or not self.team_id:
+            values['team_id'] = self.env['crm.team'].with_context(
+                default_team_id=self.partner_id.team_id.id
+            )._get_default_team_id(domain=['|', ('company_id', '=', self.company_id.id), ('company_id', '=', False)],
+                                   user_id=user_id)
+        self.update(values)
+
 
 class SaleReturnLine(models.Model):
     _name = 'sale.return.line'
@@ -488,19 +528,22 @@ class SaleReturnLine(models.Model):
     discount = fields.Float(string='Discount (%)', digits='Discount', default=0.0)
 
     product_id = fields.Many2one(
-        'product.product', string='Product', required=True ,
+        'product.product', string='Product', required=True,
         domain="[('sale_ok', '=', True), '|', ('company_id', '=', False), ('company_id', '=', company_id)]",
         change_default=True, ondelete='restrict', check_company=True)  # Unrequired company
     product_template_id = fields.Many2one(
         'product.template', string='Product Template',
         related="product_id.product_tmpl_id", domain=[('sale_ok', '=', True)])
-    product_updatable = fields.Boolean(string='Can Edit Product', readonly=True,  default=True)
+    product_updatable = fields.Boolean(string='Can Edit Product', readonly=True, default=True)
 
     product_uom_qty = fields.Float(string='Quantity', digits='Product Unit of Measure', required=False, default=1.0)
     product_uom = fields.Many2one('uom.uom', string='Unit of Measure',
                                   domain="[('category_id', '=', product_uom_category_id)]")
     product_uom_category_id = fields.Many2one(related='product_id.uom_id.category_id', readonly=True)
-    # product_custom_attribute_value_ids = fields.One2many('product.attribute.custom.value', 'sale_return_line_id', string="Custom Values")
+    # product_custom_attribute_value_ids = fields.One2many('product.attribute.custom.value', 'sale_return_line_id',
+    #                                                      string="Custom Values")
+    # product_no_variant_attribute_value_ids = fields.Many2many('product.template.attribute.value', string="Extra Values",
+    #                                                           ondelete='restrict')
 
     # M2M holding the values of product.attribute with create_variant field set to 'no_variant'
     # It allows keeping track of the extra_price associated to those attribute values and add them to the SO line description
@@ -508,11 +551,84 @@ class SaleReturnLine(models.Model):
     order_partner_id = fields.Many2one(related='order_id.partner_id', store=True, string='Customer', readonly=False)
     company_id = fields.Many2one('res.company', string='Company', required=True, default=lambda self: self.env.company)
 
+    def _get_display_price(self, product):
+        # TO DO: move me in master/saas-16 on sale.order
+        # awa: don't know if it's still the case since we need the "product_no_variant_attribute_value_ids" field now
+        # to be able to compute the full price
+
+        # it is possible that a no_variant attribute is still in a variant if
+        # the type of the attribute has been changed after creation.
+        # no_variant_attributes_price_extra = [
+        #     ptav.price_extra for ptav in self.product_no_variant_attribute_value_ids.filtered(
+        #         lambda ptav:
+        #         ptav.price_extra and
+        #         ptav not in product.product_template_attribute_value_ids
+        #     )
+        # ]
+        # if no_variant_attributes_price_extra:
+        #     product = product.with_context(
+        #         no_variant_attributes_price_extra=tuple(no_variant_attributes_price_extra)
+        #     )
+
+        if self.order_id.pricelist_id.discount_policy == 'with_discount':
+            return product.with_context(pricelist=self.order_id.pricelist_id.id, uom=self.product_uom.id).price
+        product_context = dict(self.env.context, partner_id=self.order_id.partner_id.id, date=self.order_id.date_order,
+                               uom=self.product_uom.id)
+
+        final_price, rule_id = self.order_id.pricelist_id.with_context(product_context).get_product_price_rule(
+            product or self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id)
+        base_price, currency = self.with_context(product_context)._get_real_price_currency(product, rule_id,
+                                                                                           self.product_uom_qty,
+                                                                                           self.product_uom,
+                                                                                           self.order_id.pricelist_id.id)
+        if currency != self.order_id.pricelist_id.currency_id:
+            base_price = currency._convert(
+                base_price, self.order_id.pricelist_id.currency_id,
+                self.order_id.company_id or self.env.company, self.order_id.date_order or fields.Date.today())
+        # negative discounts (= surcharge) are included in the display price
+        return max(base_price, final_price)
+
     @api.onchange('product_id')
     def _onchange_product_id(self):
-        self.price_unit = self.product_id.list_price
-        self.product_uom = self.product_id.uom_id.id
-        self.name = self.product_id.name
+        if not self.product_id:
+            return
+
+        # valid_values = self.product_id.product_tmpl_id.valid_product_template_attribute_line_ids.product_template_value_ids
+        # remove the is_custom values that don't belong to this template
+        # for pacv in self.product_custom_attribute_value_ids:
+        #     if pacv.custom_product_template_attribute_value_id not in valid_values:
+        #         self.product_custom_attribute_value_ids -= pacv
+        #
+        # # remove the no_variant attributes that don't belong to this template
+        # for ptav in self.product_no_variant_attribute_value_ids:
+        #     if ptav._origin not in valid_values:
+        #         self.product_no_variant_attribute_value_ids -= ptav
+
+        vals = {}
+        if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
+            vals['product_uom'] = self.product_id.uom_id
+            vals['product_uom_qty'] = self.product_uom_qty or 1.0
+
+        product = self.product_id.with_context(
+            lang=get_lang(self.env, self.order_id.partner_id.lang).code,
+            partner=self.order_id.partner_id,
+            quantity=vals.get('product_uom_qty') or self.product_uom_qty,
+            date=self.order_id.date_order,
+            pricelist=self.order_id.pricelist_id.id,
+            uom=self.product_uom.id
+        )
+
+        if self.order_id.pricelist_id and self.order_id.partner_id:
+            vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(
+                self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+
+        # self.price_unit = self.product_id.list_price
+        # self.product_uom = self.product_id.uom_id.id
+        # self.name = self.product_id.name
+        if not self.name:
+            vals['name'] = self.product_id.name
+
+        self.update(vals)
 
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
     def _compute_amount(self):
@@ -528,3 +644,33 @@ class SaleReturnLine(models.Model):
                 'price_total': taxes['total_included'],
                 'price_subtotal': taxes['total_excluded'],
             })
+
+    # def _get_sale_order_line_multiline_description_variants(self):
+    #     """When using no_variant attributes or is_custom values, the product
+    #     itself is not sufficient to create the description: we need to add
+    #     information about those special attributes and values.
+    #
+    #     :return: the description related to special variant attributes/values
+    #     :rtype: string
+    #     """
+    #     if not self.product_custom_attribute_value_ids and not self.product_no_variant_attribute_value_ids:
+    #         return ""
+    #
+    #     name = "\n"
+    #
+    #     custom_ptavs = self.product_custom_attribute_value_ids.custom_product_template_attribute_value_id
+    #     no_variant_ptavs = self.product_no_variant_attribute_value_ids._origin
+    #
+    #     # display the no_variant attributes, except those that are also
+    #     # displayed by a custom (avoid duplicate description)
+    #     for ptav in (no_variant_ptavs - custom_ptavs):
+    #         name += "\n" + ptav.with_context(lang=self.order_id.partner_id.lang).display_name
+    #
+    #     # Sort the values according to _order settings, because it doesn't work for virtual records in onchange
+    #     custom_values = sorted(self.product_custom_attribute_value_ids,
+    #                            key=lambda r: (r.custom_product_template_attribute_value_id.id, r.id))
+    #     # display the is_custom values
+    #     for pacv in custom_values:
+    #         name += "\n" + pacv.with_context(lang=self.order_id.partner_id.lang).display_name
+    #
+    #     return name
